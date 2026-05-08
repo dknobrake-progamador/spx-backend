9b8f0622ccff16d2e4d3b57041ca3111eda947a7
dknobrake-programador <dknobrake@gmail.com>
2026-05-07 22:52:27 -0300

    Sobe ainda mais o botao azul na tela3-imagem

--- app/tela3-imagem.tsx
+++ app/tela3-imagem.tsx
@@ -239,7 +239,7 @@ const styles = StyleSheet.create({
 
   centerBlueButton: {
     position: "absolute",
-    top: "16%",
+    top: "9%",
     left: "50%",
     transform: [{ translateX: -28 }, { translateY: -28 }],
     width: 56,
